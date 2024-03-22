77ee3e790f52a362f362a22dc44614a5a0053292
Hriday-paul <hridoychandrapaul.10@gmail.com>
2024-03-22 09:53:21 +0600

    added all file

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { io } from 'socket.io-client';
+
+function App() {
+  const serverUrl = 'http://localhost:3000';
+  const socket = io(serverUrl);
+
+// receive message depand on your uniq id
+const myId = 'myId123'
+const fid = 'hriday1';
+
+  socket.on(myId, (msg)=>{
+    console.log(msg);
+  })
+
+  const sendMessage = (e)=>{
+    e.preventDefault();
+    const message = e.target.txt.value;
+    // send a message to server
+    socket.emit('sendMessage', message, fid);
+  }
+
+  return (
+    <>
+      <form onSubmit={sendMessage}>
+        <input type="text" name='txt' id='txt' placeholder='type message...' style={{padding : '9px', borderRadius : 5}}/>
+        <button type='submit'>Send</button>
+      </form>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const serverio = require('socket.io');
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = serverio(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', socket => {
+    console.log('connected');
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+    });
+
+    //receive message 
+    socket.on('sendMessage', async (messageInfo, recever) => {
+        console.log(messageInfo, recever);
+        // send message by uniq id
+        await socket.broadcast.emit(recever, messageInfo);
+        
+    });
+});
+
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
